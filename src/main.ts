a4486969b544e1d466ad74257878afec6b13f242
alexlehner86 <xander_lehner@gmx.at>
2024-03-21 08:33:26 +0100

    create card1

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import { Card1Component } from './app/card1/card1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [Card1Component],
   template: `
     <h1>Hello from {{ name }}!</h1>
     <a target="_blank" href="https://angular.dev/overview">
